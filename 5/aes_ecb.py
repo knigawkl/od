7978ee06c1a81119f41281d5c5cf339c6e12b654
knigawkl <lukasz.knigawka@outlook.com>
2019-10-30 23:23:48 +0100

    fix

--- 5/aes_ecb.py
+++ 5/aes_ecb.py
@@ -13,6 +13,13 @@ def xor64(a, b):
     return block
 
 
+plain_text = "alamakot" * 1000
+key = "haslo123haslo123"
+block_size = 16
+no_blocks = int(len(plain_text) / block_size)
+plain_text = bytearray(plain_text, 'utf-8')
+
+
 def en_mapper(i):
     offset = i * block_size
     block = bytes(shared_data[offset:offset + block_size])
@@ -23,25 +30,7 @@ def en_mapper(i):
     return i
 
 
-def de_mapper(i):
-    offset = i * block_size
-    block = bytes(shared_data[offset:offset + block_size])
-    for j in range(1000):
-        decrypted = aes.decrypt(block)
-        block = decrypted
-    output_data[offset:offset + block_size] = bytearray(decrypted)
-    return i
-
-
-plain_text = "alamakot" * 1000
-key = "haslo123haslo123"
-iv = get_random_bytes(16)
-block_size = 16
-no_blocks = int(len(plain_text) / block_size)
-plain_text = bytearray(plain_text, 'utf-8')
-
-
-aes = AES.new(key)
+aes = AES.new(key, AES.MODE_ECB)
 shared_data = multiprocessing.RawArray(ctypes.c_ubyte, plain_text)
 output_data = multiprocessing.RawArray(ctypes.c_ubyte, plain_text)
 pool = multiprocessing.Pool(8)
@@ -50,12 +39,43 @@ pool.map(en_mapper, range(no_blocks))
 print('ECB encrypt time parallel: ', (time.time() - starttime))
 encrypted = bytes(output_data)
 
+'''
+def de_mapper(i):
+    offset = i * block_size
+    block = bytes(share_data[offset:offset + block_size])
+    for j in range(1000):
+        decrypted = aes.decrypt(block)
+        block = decrypted
+    out_data[offset:offset + block_size] = bytearray(decrypted)
+    return i
 
-shared_data = multiprocessing.RawArray(ctypes.c_ubyte, plain_text)
-output_data = multiprocessing.RawArray(ctypes.c_ubyte, plain_text)
+
+aes = AES.new(key, AES.MODE_ECB)
+share_data = multiprocessing.RawArray(ctypes.c_ubyte, plain_text)
+out_data = multiprocessing.RawArray(ctypes.c_ubyte, plain_text)
 pool = multiprocessing.Pool(8)
 starttime = time.time()
 pool.map(de_mapper, range(no_blocks))
 print('ECB Decrypt time para: ', (time.time() - starttime))
-decrypted = bytes(output_data)
+decrypted = bytes(out_data)
+print(decrypted.decode('latin-1'))
+'''
+
+
+def decrypt_ECB_serial(key, encrypted_block):
+    vector = bytearray(encrypted_block)
+    aes = AES.new(key, AES.MODE_ECB)
+    for i in range(no_blocks):
+        offset = i*block_size
+        block = encrypted_block[offset:offset+block_size]
+        for j in range(1000):
+            decrypted = aes.decrypt(block)
+            block = decrypted
+        vector[offset:offset+block_size] = bytearray(decrypted)
+    return bytes(vector)
+
+
+starttime = time.time()
+decrypted = decrypt_ECB_serial(key, encrypted)
+print('ECB Decrypt time serial: ', (time.time() - starttime))
 print(decrypted.decode('latin-1'))
---
+++ 5/old.py
@@ -0,0 +1,47 @@
+import sys
+import hashlib
+
+def hash_check(n1, n2, howManyBytes):
+    h1 = hashlib.sha256(n1.encode()).hexdigest()
+    h2 = hashlib.sha256(n2.encode()).hexdigest()
+    print(h1[:howManyBytes], h1[howManyBytes:] )
+    print(h2[:howManyBytes], h2[howManyBytes:] )
+
+def match(c, howManyBytes):
+    base = {}
+    data = f'{c}'
+    limit = 200000
+
+    for i in range(1, limit + 1):
+        xhash = hashlib.sha256(data.encode()).hexdigest()[:howManyBytes]
+        if xhash in base.keys():
+            # print(base)
+            print('Gotcha!')
+            print('Hash:', xhash, 'Index:', base.get(xhash))
+            print('Current index:', i)
+            return base.get(xhash), i
+        else:
+            base[xhash] = i
+            data += c
+    sys.exit('meh')
+
+
+char = 'a'
+howManyBytes = 8
+
+print('Character:', char, '\nBytes to match:', howManyBytes, '\nGenerating hashes...\n')
+n1, n2 = match(char, howManyBytes)
+
+s1 = char * n1
+s2 = char * n2
+
+print('\nChecking...')
+hash_check(s1, s2, howManyBytes)
+
+with open('plik1.txt', 'w+') as file:
+    file.write(s1)
+
+with open('plik2.txt', 'w+') as file:
+    file.write(s2)
+
+print('\nStrings saved to plik1 and plik2. Now you can sha256sum plik1.txt plik2.txt')
