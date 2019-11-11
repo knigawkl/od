19a32bd8231b74306e6d0e7d78b13b99bcb8724e
knigawkl <lukasz.knigawka@outlook.com>
2019-11-11 20:52:10 +0100

    more

---
+++ 7/pin1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TAJNY_PIN 1234
+int main(int ac, char **av)
+{
+    int pin;
+    if (av[1][0] == '-') {
+        printf("Atak zablokowany\n");
+        exit(1);
+    } 
+    sscanf(av[1], "%4d", &pin);
+    printf("%d\n", pin);
+    if (pin == TAJNY_PIN) {
+        pin = -1;
+    }
+    if (pin == -1) {
+        printf("PIN poprawny\n");
+    } else {
+        printf("PIN bledny\n");
+    }
+}
+
